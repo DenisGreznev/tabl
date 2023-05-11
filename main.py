bb2bcfc1f087af9b10a6d1b6afe81e965b6880db
DenisGreznev <91825846+DenisGreznev@users.noreply.github.com>
2023-05-11 17:57:01 +0300

    main1

---
+++ main.py
@@ -0,0 +1,29 @@
+# Печать таблицы умножения
+print("Multiplication Table in Hexadecimal:")
+for i in range(1, 16):
+    row = ''
+    for j in range(1, 16):
+        result = ((i * j))
+        hex_result = hex(result).upper()
+        hex_result = hex_result[2:]
+        if len(hex_result) < 2:
+            hex_result = '0' + str(hex_result)
+            row += hex_result + "\t"
+        else:
+            row += hex_result + "\t"
+    print(row)
+
+# Печать таблицы сложения
+print("Addition Table in Hexadecimal:")
+for i in range(1, 16):
+    row = ''
+    for j in range(1, 16):
+        result = i + j
+        hex_result = hex(result).upper()
+        hex_result = hex_result[2:]
+        if len(hex_result) < 2:
+            hex_result = '0' + str(hex_result)
+            row += hex_result + "\t"
+        else:
+           row += hex_result + "\t"
+    print(row)
